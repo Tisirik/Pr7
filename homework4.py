066cf2427165c2ad65a256276c9be0881c98a0b0
Tisirik <timik.siraev@gmail.com>
2024-11-10 21:36:50 +0500

    Созданны каталоги

--- homework4.py
+++
@@ -1,8 +0,0 @@
-print("Решение урравнения: x = a*b*b / c")
-a = input("Введите целое значение переменной a: ")
-b = input("Введите целое значение переменной b: ")
-c = input("Введите целое значение переменной c (c не равно 0): ")
-if a.isdigit() and b.isdigit() and c.isdigit() and int(c)!=0:
-    print("Число x равно: ", int(a)*int(b)*int(b)/int(c))
-else:
-    print('Некорректный ввод!')
